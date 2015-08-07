e63792645e59c807a5102ff38615535851e756f0
Jacob Alheid <shakefu@gmail.com>
2015-08-07 14:12:25 -0700

    More tests, more working code.

--- config.js
+++ config.js
@@ -2,10 +2,14 @@ var _ = require('lodash');
 var assert = require('assert');
 
 
-
-var Config = function Config () {
+/**
+ * Create a new config instance backed by etcd
+ */
+var Config = function Config (hosts, opts) {
     assert(this instanceof Config, "Missing 'new' keyword");
+    this.init(hosts, opts);
 };
+exports.Config = Config;
 
 /**
  * Return a config value for `key`.
@@ -17,8 +21,44 @@ Config.prototype.get = function get (key, def, callback) {
     if (!callback) {
         return def;
     }
-    assert(_.isFunction(callback));
+    assert(_.isFunction(callback), "callback must be a function");
     callback(null, def);
 };
 
-exports.Config = Config;
+
+var _getEnvHosts = function _getEnvHosts (hosts) {
+    hosts = process.env.JETCONFIG_ETCD || hosts;
+    
+    if (hosts === undefined) hosts = ['127.0.0.1:2379'];
+    if (_.isString(hosts)) hosts = hosts.split(',');
+
+    assert(_.isArray(hosts), "hosts must be string or array");
+    assert(_.reduce(_.map(hosts, _.isString)), "host must be string");
+    
+    hosts = _.map(hosts, _.trim);
+    return hosts;
+};
+
+/**
+ * Sets up the Config instnace
+ */
+Config.prototype.init = function init (hosts, opts) {
+    var defaults = {
+        prefix: 'config/',
+        ssl: {}
+    };
+    if (_.isPlainObject(hosts)) {
+        opts = hosts;
+        hosts = undefined;
+    }
+    opts = opts || {};
+    opts = _.defaults(opts, defaults);
+
+    assert(_.isString(opts.prefix), "prefix must be string");
+    assert(_.isPlainObject(opts.ssl), "ssl options must be object");
+
+    this.prefix = opts.prefix;
+    this.sslopts = opts.ssl;
+
+    this.hosts = _getEnvHosts(hosts);
+};
--- test/index.js
+++ test/index.js
@@ -23,10 +23,56 @@ before(function (done) {
 
 describe("Config", function () {
     describe("new", function () {
+        var host1 = '127.0.0.1:4001';
+        var host2 = '127.0.0.1:2379';
+
         it("should be required", function () {
             expect(Config).to.throw("AssertionError: Missing 'new' keyword");
         });
+
+        it("should provide a default host", function () {
+            var conf = new Config();
+            conf.hosts.should.eql([host2]);
+        });
+
+        it("should allow for env-based hosts", function () {
+            process.env.JETCONFIG_ETCD = host2;
+            var conf = new Config();
+            conf.hosts.should.eql([host2]);
+            delete process.env.JETCONFIG_ETCD;
+        });
+
+        it("should allow for a single string host", function () {
+            var conf = new Config(host2);
+            conf.hosts.should.eql([host2]);
+        });
+
+        it("should allow for array hosts", function () {
+            var conf = new Config([host1, host2]);
+            conf.hosts.should.eql([host1, host2]);
+        });
+
+        it("should throw for other data types", function () {
+            expect(function (){ new Config(1) }).to.throw( // jshint ignore:line
+                "AssertionError: hosts must be string or array");
+        });
+
+        it("should throw if host in array isnt string",  function () {
+            expect(function (){ new Config([1]); }) // jshint ignore:line
+                .to.throw("AssertionError: host must be string");
+        });
+
+        it("should allow for multiple hosts separated by comma", function () {
+            var conf = new Config(host1 + ',' + host2);
+            conf.hosts.should.eql([host1, host2]);
+        });
+
+        it("should trim comma separated hosts", function () {
+            var conf = new Config(host1 + ', ' + host2);
+            conf.hosts.should.eql([host1, host2]);
+        });
     });
+
     describe('#get()', function (){
         var conf;
 
